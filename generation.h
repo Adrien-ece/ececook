b8740326264fab5e1129e45e99597883223370fc
Adrien-ece <149948263+Adrien-ece@users.noreply.github.com>
2024-05-17 17:18:36 +0200

    Create generation.h

---
+++ generation.h
@@ -0,0 +1,13 @@
+//
+// Created by FA706 on 05/04/2024.
+//
+
+#ifndef PROJET_S2_GENERATION_H
+#define PROJET_S2_GENERATION_H
+#include <allegro.h>
+SAMPLE* charger_musique(const char* chemin);
+BITMAP* charger_image(const char* chemin);
+
+
+
+#endif //PROJET_S2_GENERATION_H
