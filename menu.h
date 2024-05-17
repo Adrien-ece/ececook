e80adeee3a6001fdd09c747bde09dfededbdb95c
Adrien-ece <149948263+Adrien-ece@users.noreply.github.com>
2024-05-17 17:19:26 +0200

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,12 @@
+//
+// Created by FA706 on 05/04/2024.
+//
+
+#ifndef PROJET_S2_MENU_H
+#define PROJET_S2_MENU_H
+#include "allegro.h"
+#include <stdio.h>
+void afficherMenu();
+
+
+#endif //PROJET_S2_MENU_H
