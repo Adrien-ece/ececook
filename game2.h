62e6e32cdd60638719804c3f8bbb5f1a51297433
Adrien-ece <149948263+Adrien-ece@users.noreply.github.com>
2024-05-19 21:53:01 +0200

    Create game2.h

---
+++ game2.h
@@ -0,0 +1,11 @@
+#ifndef GAME2_H
+#define GAME2_H
+
+#include "game.h"
+
+// Déclarations des fonctions spécifiques au niveau 2
+void initialiserCommandesNiveau2();
+void afficherCommandesNiveau2();
+void jouerNiveau2();
+
+#endif /* GAME2_H */
